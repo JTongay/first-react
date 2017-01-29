7667c1eb21318be1194e837df0fa338884df1edf
Joseph Tongay <dirk.tongay@gmail.com>
2017-01-28 19:36:05 -0600

    totally just guessed on how to add a component into another one

---
+++ src/Test.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+var Test = React.createClass({
+
+  render: function() {
+    return (
+      <h1>Booyah</h1>
+      // <div />
+    );
+  }
+
+});
+
+module.exports = Test;
